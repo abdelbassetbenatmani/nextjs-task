d0b02d49a52cd17bcd47e12ce075363aeea8fcd1
abdelbassetbenatmani <abdelbassetbenatmani@gmail.com>
2023-12-22 17:12:22 +0100

    fix bugs

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -12,7 +12,6 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Label } from "@/components/ui/label";
 
-
 interface Props {
   params: any;
 }
@@ -22,27 +21,33 @@ const UserInfo = async ({ params }: Props) => {
 
   return (
     <Card className="w-[90%] md:w-[500px] mx-auto mt-10">
-      
       <CardHeader className="text-center border-b">
-      <Image
-        src="/logo.png"
-        alt="logo"
-        width={150}
-        height={150}
-        className="mx-auto dark:invert"
-      />
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width={150}
+          height={150}
+          className="mx-auto dark:invert"
+        />
         <CardTitle>Your information</CardTitle>
-        <CardDescription>You can update each information by click update button</CardDescription>
+        <CardDescription>
+          You can update each information by click update button
+        </CardDescription>
       </CardHeader>
       <CardContent className=" pt-4 space-y-2">
-
-        <Label className="text-base" >Hi,</Label>
+        <Label className="text-base">Hi,</Label>
         <p className=" text-xl font-semibold">{data?.name}</p>
-        <Label className="text-base" >You are in sector</Label>
+        <br />
+        <Label className="text-base mb-4">You are in sector</Label>
         <p className=" text-xl font-semibold">{data?.sector.name}</p>
-
-        <p>{data?.agree ? <p className=" text-xl font-semibold">"You are agree to term"</p>:""}</p>
-       
+        <br />
+        <p>
+          {data?.agree && (
+            <span className=" text-xl font-semibold">
+              &quot;You are agree to term&quot;
+            </span>
+          )}
+        </p>
       </CardContent>
       <CardFooter className="flex justify-between">
         <Link href={`/users/${data?.id}/update`}>
