9590be7f3c765bc29ef681ed6a2a7c479913a61d
abdelbassetbenatmani <abdelbassetbenatmani@gmail.com>
2023-12-22 17:47:16 +0100

    redesign navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import React from 'react'
 import Switcher from './switcher'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className='h-[90px] px-4 flex items-center justify-between w-full shadow-lg border-b bg-white dark:bg-gray-900'>
         <div>
+        <Link href="/">
         <Image src="/logo.png" alt="logo" width={60} height={60} className="mx-auto dark:invert"/>
+        </Link>
         </div>
          <Switcher/>
     </div>
